c2b9b9b79b9cb05ca71ae1aa29f2fee1782a6278
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-24 07:48:36 +0900

    Title: 3 또는 5의 배수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250624/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >>a;
+    if(a % 3==0){
+        cout<< "Yes"<<endl;
+    }
+    else
+    cout <<"No"<<endl;
+    if(a % 5==0){
+        cout<< "Yes";
+    }
+    else
+    cout <<"No";
+    return 0;
+}
