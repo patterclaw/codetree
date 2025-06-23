8782fd691cc2ba6965b0d2774552aa51f7dfa580
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-24 07:31:19 +0900

    Title: 알파벳에 따른 평가, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250624/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char c;
+    cin >> c;
+    if(c=='S')
+    cout << "Superoir";
+    if(c=='A')
+    cout << "Excellent";
+    if(c=='B')
+    cout << "Good";
+    if(c=='C')
+    cout << "Usually";
+    if(c=='D')
+    cout << "Effort";
+    else
+    cout << "Failure";    
+    return 0;
+}
