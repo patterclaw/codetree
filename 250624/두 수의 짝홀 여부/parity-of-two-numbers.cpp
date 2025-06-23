24267478db32aac7cf6e0540d799d3cafa961b2a
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-24 07:44:28 +0900

    Title: 두 수의 짝홀 여부, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250624/두 수의 짝홀 여부/parity-of-two-numbers.cpp
+++ 250624/두 수의 짝홀 여부/parity-of-two-numbers.cpp
@@ -6,13 +6,13 @@ int main() {
     int a,b;
     cin >> a>>b;
     if(a%2==0)
-    cout << "even";
+    cout << "even"<< endl;
     else
-    cout << "odd";
+    cout << "odd"<<endl;
     if(b%2==0)
-    cout << "even";
+    cout << "even"<<endl;
     else
-    cout << "odd";
+    cout << "odd"<<endl;
     
     return 0;
 }
