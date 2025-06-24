b5f02d9942afb2c0f14e228b5fbd3d67f74658c5
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-24 15:01:29 +0900

    Title: 비교 연산, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250624/비교 연산/comparison-operator.cpp
+++ 250624/비교 연산/comparison-operator.cpp
@@ -5,12 +5,12 @@ int main() {
     // Please write your code here.
     int a,b;
     cin >> a>>  b;
-    cout << (a>=b);
-    cout << (a>b);
-    cout << (b>=a);
-    cout << (b>a);
-    cout << (a==b);
-    cout <<(a!=b);
+    cout << (a>=b)"\n";
+    cout << (a>b)"\n";
+    cout << (b>=a)"\n";
+    cout << (b>a)"\n";
+    cout << (a==b)"\n";
+    cout << (a!=b)"\n";
 
     return 0;
 }
