3ddc7f1b5aa576b6504cf38a3f7d81aecb34add9
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-24 16:37:30 +0900

    Title: 두 사람, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250624/두 사람/two-person.cpp
+++ 250624/두 사람/two-person.cpp
@@ -6,7 +6,7 @@ int main() {
     int a,b;
     char m,w;
     cin >>a>>m>>b>>w;
-    if((a>=19 || m=='M') && (b>=19 || w=='M'))
+    if((a>=19 && m=='M') || (b>=19 && w=='M'))
     cout <<1;
     else 
     cout <<0;
