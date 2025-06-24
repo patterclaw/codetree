061edbd34fc410cb4672dba98445e10a94fd26c8
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-24 15:00:08 +0900

    Title: 비교 연산, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250624/비교 연산/comparison-operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin >> a>>  b;
+    cout << (a>=b);
+    cout << a>b;
+    cout << b>=a;
+    cout << b>a;
+    cout << a==b;
+    cout <<a!=b;
+
+    return 0;
+}
