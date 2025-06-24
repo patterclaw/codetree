ab23612cb8aa77d59d3e59e70b0f65f98959a8bd
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-24 15:12:34 +0900

    Title: 점수 비교, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250624/점수 비교/score-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    int x,y;
+    cin >> a>> b;
+    cin >>x>> y;
+    cout<< (a>x)&&(b>y);
+
+    return 0;
+}
