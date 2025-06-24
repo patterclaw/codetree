f322cdee8a7c1845a2a7018ab9e0f019c9d68ba7
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-25 03:39:05 +0900

    Title: 감소시키며 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250625/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a=26;
+    while(a>=10)
+    {
+        cout << a << " ";
+        a--;
+    }
+    return 0;
+}
