f26d7b98885aa83be08865a1643885c089f57db3
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-24 15:06:59 +0900

    Title: 4가지 관계연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250624/4가지 관계연산자/four-relational-operators.cpp
+++ 250624/4가지 관계연산자/four-relational-operators.cpp
@@ -6,7 +6,7 @@ int main() {
     int a,b;
     cin >>a>>b;
     cout << (a>=b)<<endl;
-    cout << (a>=b) <<endl;
+    cout << (a>b) <<endl;
     cout << (b>=a)<<endl;
     cout << (b>a) <<endl;
     return 0;
