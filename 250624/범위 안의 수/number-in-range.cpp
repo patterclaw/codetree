9500890d909418da6b5539bab7f10be051da448c
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-24 15:15:54 +0900

    Title: 범위 안의 수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250624/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    if(10<=a && a<=20){
+        cout << "yes";
+    }
+    else
+    cout  << "no";
+    return 0;
+}
