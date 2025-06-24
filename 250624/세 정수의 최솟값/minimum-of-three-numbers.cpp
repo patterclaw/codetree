97866c772178becef672d1f959cc091eda92e43f
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-24 15:26:09 +0900

    Title: 세 정수의 최솟값, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250624/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    cin >>a>>b>>c;
+    int min=a;
+    if(min>b)
+    min=b;
+    else if(min >c)
+    min=c;
+    cout << min;
+    return 0;
+}
