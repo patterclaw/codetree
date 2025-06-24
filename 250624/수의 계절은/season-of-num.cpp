b69411629a554ecec8491bc952d638d49345db49
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-24 16:12:52 +0900

    Title: 수의 계절은, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250624/수의 계절은/season-of-num.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int m;
+    cin >>m;
+    if(3<=m && m<=5)
+    cout << "Spring";
+    else if(6<=m && m<=8)
+    cout << "Summer";
+    else if(9<=m && m<=11)
+    cout << "Fall";
+    else 
+    cout <<"Winter"    ;
+    return 0;
+}
