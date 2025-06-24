0786ed9ae82cffa13f4da520f6b496b47b42ee95
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-24 19:36:42 +0900

    Title: 홀수만 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250624/홀수만 출력/output-only-odd.cpp
+++ 250624/홀수만 출력/output-only-odd.cpp
@@ -5,7 +5,7 @@ int main() {
     // Please write your code here.
     int a,b;
     cin >> a>>b;
-    for(int i>=a;i<=b;i++){
+    for(int i=a;i<=b;i++){
         if(i%2!=0){
             cout << i << " ";
         }
