29a0dc37b2c85b124d9d79210fb8e6eb7eb4d518
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-25 03:40:25 +0900

    Title: n부터 1까지 정수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250625/n부터 1까지 정수 출력/print-n-to-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    
+    while(a>0)
+    {
+        cout << a << " ";
+        a--;
+    }
+    return 0;
+}
