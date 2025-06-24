55e57e58b4b91418df983ca08e6a6fc997335da6
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-24 19:41:36 +0900

    Title: B부터 A까지 감소, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250624/B부터 A까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin >> a>> b;
+    for(int i=b;i>a-1;i--)
+    cout <<i << " ";
+    return 0;
+}
