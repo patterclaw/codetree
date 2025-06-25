a1f4166341a585f76027bbede959967ed7763be9
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-25 14:51:09 +0900

    Title: 배수의 개수 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250625/배수의 개수 2/number-of-multipliers-2.cpp
+++ 250625/배수의 개수 2/number-of-multipliers-2.cpp
@@ -6,9 +6,8 @@ int main() {
     int n,cnt=0;
     for(int i=0;i<10;i++){
         cin>>n;
-        if(i%2!=0){
+        if(n%2!=0){
             cnt++;
-            
         }
     }
     cout << cnt << endl; 
