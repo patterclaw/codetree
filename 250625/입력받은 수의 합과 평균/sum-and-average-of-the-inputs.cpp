4119b66fa41b3c80798ee45783889231145977ff
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-25 16:43:42 +0900

    Title: 입력받은 수의 합과 평균, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250625/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >>n;
+    int sum=0;
+    double avg=0;
+    for(int i=0;i<n;i++){
+        int a;
+        cin >>a;
+        sum+=a;
+
+    }
+    avg=(double)sum/n;
+    cout << sum << " " << avg;
+    return 0;
+}
