9abfc02170c53936b503c1e27561e2ff7eef370d
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-25 16:26:19 +0900

    Title: 정수 사이의 배수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250625/정수 사이의 배수/multiplication-between-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    int sum=0,cnt=0;
+    double average=0;
+    cin >>a>>b;
+    cout<<fixed;
+    cout.precision(1);
+    for(int i=a;i<=b;i++){
+        if(i%5==0 || i%7==0){
+            sum+=i;
+            cnt++;
+        }
+    }
+    average=(double)sum/cnt;
+    cout << sum <<average;
+    return 0;
+}
