1d55039c57b56b562fc28d8694e893157cd76f46
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-26 13:40:13 +0900

    Title: 20대의 평균 나이, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250626/20대의 평균 나이/average-age-of-20.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int sum=0,cnt=0,a;
+    double avg=0;
+    while(1){
+        cin >> a;
+        if(a<20 || a>29)
+        break;
+        cnt++;
+        sum+=a;
+
+    }
+    cout<<fixed;
+    cout.precision(2);
+    avg=(double)sum/cnt;
+    return 0;
+}
