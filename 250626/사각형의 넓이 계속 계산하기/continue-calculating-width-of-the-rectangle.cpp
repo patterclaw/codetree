5a39c219d1219d9eebc79cead6f4610c1c7e2d7a
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-26 13:34:09 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250626/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    char c;
+    while(true){
+        cin >> a >>  b >> c;
+        cout << a*b <<endl;
+        if(c=='C')
+        {
+        break;}
+        
+    }
+    return 0;
+}
