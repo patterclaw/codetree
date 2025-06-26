1fd39dd8bd8a5298d4b761d3b945c84eb6178dd5
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-26 12:32:07 +0900

    Title: 조건에 부합하는 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250626/조건에 부합하는 수/number-that-meets-the-condition.cpp
+++ 250626/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -6,10 +6,9 @@ int main() {
     int a;
     cin >> a;
     for(int i=1;i<=a;i++){
-        if((i%2==0 && i%4!=0) || (i%7)<4)
+        if((i%2==0 && i%4!=0) || (i%7)<4 || i/8==0 || i/8==2 || i/8==4 || i/8==6 || i/8==8 || i/8==10 || i/8==12)
         continue;
-        else if(i/8==0 || i/8==2 || i/8==4 || i/8==6 || i/8==8)
-        continue;
+        
         cout << i << " ";
     }
     return 0;
