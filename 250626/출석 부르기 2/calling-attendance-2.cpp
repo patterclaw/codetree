64bf96437d60d2eb64f44101bcf4dd071afca584
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-26 13:28:48 +0900

    Title: 출석 부르기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250626/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    for(;;){
+        cin >>n;
+        if(n==1)
+        cout << "John" <<endl;
+        else if(n==2)
+        cout << "Tom" <<endl;
+        else if(n==3)
+        cout << "Paul" <<endl;
+        else if(n==4)
+        cout << "Sam" <<endl;
+        else{
+            cout <<"Vacancy";
+            break;
+        }
+        
+    }
+    return 0;
+}
