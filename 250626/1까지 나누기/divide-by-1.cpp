8ecbd4decaddff363074b469649f5155160c5789
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-26 13:02:26 +0900

    Title: 1까지 나누기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250626/1까지 나누기/divide-by-1.cpp
+++ 250626/1까지 나누기/divide-by-1.cpp
@@ -5,13 +5,11 @@ int main() {
     // Please write your code here.
     int n,cnt=0;
     cin >>n;
-    for(int i=1;i<=n;i++){
+    for(int i=1;n>1;i++){
         n=n/i;//50나누기1=50,cnt=1 50나누기2는 25 카운트는 2
-        cnt++;
-        if(n<=1){
-        break;
-        }
+        ++cnt;
+        
     }
-    cout <<++cnt;
+    cout <<cnt;
     return 0;
 }
