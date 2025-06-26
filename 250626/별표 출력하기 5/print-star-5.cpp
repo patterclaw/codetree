7eb63a5a8692652cca5dea99fc5379fbc9077f40
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-26 21:34:39 +0900

    Title: 별표 출력하기 5, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250626/별표 출력하기 5/print-star-5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >>n;
+    for(int i=n;i>0;i--){
+        for(int j=1;j<=i*i;j++){
+        if(j==i || j==2*i || j==3i){
+            cout << "* ";
+        }
+        cout<< "*";
+
+        }
+    cout <<endl;
+ }
+    return 0;
+}
