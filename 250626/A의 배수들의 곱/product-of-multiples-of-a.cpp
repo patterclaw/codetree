1735753797a194d0a0cc41da352553bb1ffbc6ae
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-26 12:08:03 +0900

    Title: A의 배수들의 곱, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250626/A의 배수들의 곱/product-of-multiples-of-a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    int prod=1;
+    cin >> a>> b;
+    for(int i=1;i<=b;i++){
+        if(i%a==0){
+            prod*=i;
+        }
+    }
+    cout <<prod;
+    return 0;
+}
