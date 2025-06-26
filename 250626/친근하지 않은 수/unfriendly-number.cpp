f2a05f2900220cde4af47576494d656ae18c8478
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-26 12:12:50 +0900

    Title: 친근하지 않은 수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250626/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    int cnt=0;
+    cin >>n;
+    for(int i=1;i<=n;i++){
+        if(i%2==0 || i%3==0 || i%5==0)
+        continue;
+        cnt++;
+    }
+    cout <<cnt;
+
+    return 0;
+}
