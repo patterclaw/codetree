fed2a15e272b309c94345e9eaace5f9913f18b84
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-27 16:33:01 +0900

    Title: 1-9 정사각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250627/1-9 정사각형/1-9-square.cpp
+++ 250627/1-9 정사각형/1-9-square.cpp
@@ -8,7 +8,7 @@ int main() {
     cin >>n;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(cnt++==9)
+            if(cnt==9)
             cnt=0;
             cnt++;
             cout <<cnt;
