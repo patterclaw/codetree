c88218d4a8b7fee44ca5bbb594dca2f22b9bf7f8
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-27 13:43:56 +0900

    Title: 별 그리기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250627/별 그리기/star-drawing.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<n-i;j++)
+        cout << " ";
+        for(int j=0;j<2*i-1;j++)
+        cout << "*";
+        cout <<'\n';
+    }
+    for(int i=n-1;i>=1;i--){
+        for(int j=0;j<n-i;j++)
+        cout << " ";
+        for(int j=0;j<2*i-1;j++)
+        cout << "*";
+        cout <<'\n';
+    }
+    
+
+    // Please write your code here.
+
+    return 0;
+}
