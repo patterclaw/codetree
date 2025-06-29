e6e1b5778e73772ea237e873c3a5fad88c7ea865
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-29 14:30:59 +0900

    Title: 평균점수 구하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250629/평균점수 구하기/print-average.cpp
+++ 250629/평균점수 구하기/print-average.cpp
@@ -7,11 +7,13 @@ int main() {
     cout << fixed;
     cout.precision(1);
     double avg=0;
+    double sum=0;
     for(int i=0;i<8;i++){
         cin >> score[i];
-        int sum=sum+score[i];
-        avg=(double)sum/8;
+        sum+=score[i];
+        
     }
+    avg=sum/8;
     cout << avg;
     return 0;
 }
