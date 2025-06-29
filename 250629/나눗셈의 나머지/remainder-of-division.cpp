569793d58e54a51714173cbe114f8720f4087da3
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-29 22:24:32 +0900

    Title: 나눗셈의 나머지, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250629/나눗셈의 나머지/remainder-of-division.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,cnt=0;
+    int mod[100]={};
+    int c[100]={};
+    cin >>a>>b;
+    for(int i=0;i<100;i++){
+        c[i]=a%b;
+        cnt++;
+        a=a/b;
+        if(a<=1)
+        break;  
+    }
+    int sum=0;
+    for(int i=0;i<cnt;i++){
+        mod[c[i]]++;
+    }
+    for(int i=0;i<100;i++)
+    sum+=mod[i]*mod[i];
+   
+    cout << sum;
+    return 0;
+}
