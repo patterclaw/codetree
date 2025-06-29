9d527d9c510b37b10ac6dc4d50a8dcb753d779c6
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-06-29 15:08:47 +0900

    Title: 특정 위치의 수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250629/특정 위치의 수/numbers-in-specific-location.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10];
+    for(int i=0;i<=9;i++){
+        cin >> arr[i];
+    }
+    int sum=0
+    sum=arr[2]+arr[4]+arr[9];
+    cout << sum;
+    return 0;
+}
