f5949153ff2acbc9dc6d5db96d8980ff6b51b72e
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-01 15:37:02 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250701/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int price[1000];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> price[i];
+    }
+    int max=0;
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(price[j]-price[i]>max)
+            max=price[j]-price[i];
+        }
+    }
+    cout << max;
+    // Please write your code here.
+
+    return 0;
+}
