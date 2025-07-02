a8c34c57bbf12be047ad780b72a4d1f8e8a02984
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-02 16:27:21 +0900

    Title: 동전이 있는 위치, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250702/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[9][9]={};
+    int n,m;
+    cin >>n>>m;
+    for(int i=0;i<m;i++){
+        int r,c;
+        cin >> r >>c;
+        arr[r][c]=1;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(a[i][j]==1)
+            cout << 1 << " ";
+            else
+            cout <<0 << " ";
+        }
+        cout <, '\n';
+    }
+    return 0;
+}
