010319616c5051be3697e7affb75b1b4df5d6abf
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-02 17:09:34 +0900

    Title: 문자열 범위 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250702/문자열 범위 출력하기/print-string-in-range.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin,s);
+    for(int i=2;i<10;i++)
+    cout << s[i];
+    // Please write your code here.
+    return 0;
+}
