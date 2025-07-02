6bbdf67c73e09cbc37c9912e9f0d71e87677fb2d
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-02 16:11:08 +0900

    Title: 파스칼의 삼각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250702/파스칼의 삼각형/pascals-triangle.cpp
+++ 250702/파스칼의 삼각형/pascals-triangle.cpp
@@ -8,14 +8,16 @@ int main() {
     cin >> n;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(i==j)
+            if(i==j || j==0)
             arr[i][j]=1;
             else if(i>=j)
             arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+            
         }
     }
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
+            if(i>=j)
             cout  << arr[i][j] << " ";
         }cout << '\n';
     }
