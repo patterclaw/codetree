92fb56ffb461f00211ad0540b6ea8edff7800627
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-02 16:52:39 +0900

    Title: 문자열 두번 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250702/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    for(int i=0;i<2;i++){
+        cout << s << '\n';
+    }
+    // Please write your code here.
+    return 0;
+}
