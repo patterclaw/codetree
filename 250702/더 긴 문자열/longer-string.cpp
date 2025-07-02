db0e8dfec616ce6edc8c31df802d525256816314
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-02 16:55:41 +0900

    Title: 더 긴 문자열, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250702/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int s1,s2;
+    cin >>s1>>s2;
+    if(s1.length()>s2.length)
+    cout << s1 << s1.length();
+    else if(s1.length()<s2.length)
+    cout << s2 << s2.length();
+    else
+    cout <<"same";
+    return 0;
+}
