6de956f512df3a79c73a6c38bfcf042eadb4782f
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-03 18:52:33 +0900

    Title: 아스키코드의 합과 차, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250703/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a,b;
+    cin >> a>> b;
+    cout << a+b <<  " ";
+    if(a>=b)
+    cout << a-b;
+    else
+    cout << b-a;
+    // Please write your code here.
+    return 0;
+}
