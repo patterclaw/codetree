b3efc617e51906dcf3455fec0038dfc1f5db586b
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-03 16:08:49 +0900

    Title: 문자 교체하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250703/문자 교체하기/changing-char.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s1,s2;
+    cin >> s1>>s2;
+    s2[0]=s1[0];
+    s2[1]=s1[1];
+    cout <<s2;
+    // Please write your code here.
+    return 0;
+}
