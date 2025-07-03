f6c1bef9fc18baf7824f0ca036ca113f0aa3b3dd
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-03 15:07:00 +0900

    Title: 문자열 추가하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250703/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a="Hello";
+    string str;
+    cin >>str;
+    str+=a;
+    cout <<str;
+    // Please write your code here.
+    return 0;
+}
