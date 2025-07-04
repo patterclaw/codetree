fb326b3f5af0e4721f88d00db9846c29475ea5a3
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-04 17:15:20 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250704/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+void print(int n){
+    for(int i=0;i<n;i++){
+        cout << "12345^&*()_";
+        cout <<'\n';
+    }
+    
+}
+int main() {
+    cin >> N;
+    print(N);
+    // Please write your code here.
+
+    return 0;
+}
