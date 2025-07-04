13205ac568dc6b115368d3ea789401d5958d24ba
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-04 15:45:56 +0900

    Title: 미는 횟수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250704/미는 횟수/number-of-pushes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a,b;
+    cin >>a>>b;
+    int cnt=0;
+    while(a.length()!=cnt){
+        a=a.substr(1,a.length()-1)+a.substr(0,1);
+        cnt++;
+        if(a==b)
+        break;
+    }
+    if(cnt!=a.length())
+    cout << cnt;
+    else
+    cout << -1;
+
+    // Please write your code here.
+    return 0;
+}
