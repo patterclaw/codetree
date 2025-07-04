3fce37f0588ec86b50214940a9775894368e1014
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-04 14:57:08 +0900

    Title: 각 자리 숫자들의 합, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250704/각 자리 숫자들의 합/sum-of-each-digit.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >>n;
+    string s=to_string(n);
+    int sum=0;
+    for(int  i=0;i<s.length();i++){
+        sum+=s[i]-'0';
+    }
+    cout <<sum;
+    // Please write your code here.
+    return 0;
+}
