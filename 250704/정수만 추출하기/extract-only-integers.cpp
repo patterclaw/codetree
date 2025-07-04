80b1ee392be4fa5c1a457936b81b44e91da4ffcd
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-04 14:42:25 +0900

    Title: 정수만 추출하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250704/정수만 추출하기/extract-only-integers.cpp
+++ 250704/정수만 추출하기/extract-only-integers.cpp
@@ -5,14 +5,12 @@ using namespace std;
 int main() {
     string a,b;
     cin  >> a >> b;
-    int c=0,d=0;
+    int c=a.length(),d=b.length();
     for(int i=0;i<a.length();i++){
         if(!('0'<=a[i] && '9'>=a[i])){
             c=i;
             break;
         }
-       
-        
     }
     a=a.substr(0,c);
     for(int i=0;i<b.length();i++){
@@ -21,7 +19,6 @@ int main() {
         d=i;
         break;
        }
-        
     }
     b=b.substr(0,d);
     cout << stoi(a)+stoi(b);
