9e4c2e1cc2e931a4280fe606f38cee7177b6457e
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-05 20:07:31 +0900

    Title: 함수를 이용한 온전수 판별, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250705/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+bool completeNumber(int a){
+    if(a%2==0 || a%10==5 || (a%3==0 && a%9!=0))
+    return false;
+
+    return true;
+}
+
+
+int main() {
+    int a,b,cnt=0;
+    cin >> a >> b;
+    for(int i=a;i<=b;i++){
+        if(completeNumber(i)){
+            cnt++;
+        }
+    }
+    cout <<cnt;
+
+    // Please write your code here.
+
+    return 0;
+}
