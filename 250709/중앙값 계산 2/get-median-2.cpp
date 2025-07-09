bef66a409091cb90fca5eb79106b1608ba90c708
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-09 13:21:10 +0900

    Title: 중앙값 계산 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250709/중앙값 계산 2/get-median-2.cpp
+++ 250709/중앙값 계산 2/get-median-2.cpp
@@ -10,7 +10,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
         if(i%2==0){
-        sort(arr,arr+i);
+        sort(arr,arr+i+1);
         cout << arr[i/2] << " ";
         }
     }
