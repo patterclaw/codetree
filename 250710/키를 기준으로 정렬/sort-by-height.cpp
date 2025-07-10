e3b1eb555c3975e7fa6c076e277e81236e19556a
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-10 22:53:43 +0900

    Title: 키를 기준으로 정렬, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250710/키를 기준으로 정렬/sort-by-height.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+class Student{
+    public: 
+    string name;
+    int height;
+    int weight;
+    Student(string name,int height,int weight){
+        this->name=name;
+        this->height=height;
+        this->weight=weight;
+    }
+    Student(){}
+};
+bool cmp(const Student &a,const Student &b){
+    return a.height<b.height;
+}
+int main() {
+    cin >> n;
+    string name;
+    int height;
+    int weight;
+    Student student[5];
+    for (int i = 0; i < n; i++) {
+        cin >> name;
+        cin >> height;
+        cin >> weight;
+        student[i]=Student(name,height,weight);
+    }
+    sort(student,student+n,cmp);
+    for(int i=0;i<n;i++){
+        cout << student[i].name << " " <<student[i].height << " " << student[i].weight << '\n';
+    }
+    
+    // Please write your code here.
+
+    return 0;
+}
