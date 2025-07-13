590cbe4514140e4c6e4cd4796644f0176a3bd61b
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-13 20:28:41 +0900

    Title: 원점으로부터의 거리, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250713/원점으로부터의 거리/distance-from-origin.cpp
+++ 250713/원점으로부터의 거리/distance-from-origin.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int N;
 int x[1000];
 int y[1000];
-int sum[1000];
+
 class Coordinate{
     public:
     int x;
@@ -32,14 +32,13 @@ int main() {
     Coordinate c[1000];
     for (int i = 0; i < N; i++) {
         cin >> x[i] >> y[i];
-        if(x[i]<0){
-            if(y[i]<0){
-                y[i]=-y[i];
-            }
-            x[i]=-x[i];
-        }
-        sum[i]=x[i]+y[i];
-        c[i]=Coordinate(x[i],y[i],sum[i],i+1);
+        int abs1=x[i],abs2=y[i];
+        if(x[i]<0)
+        abs1=-x[i];
+        if(y[i]<0)
+        abs2=-y[i];
+        int sum=abs1+abs2;
+        c[i]=Coordinate(x[i],y[i],sum,i+1);
     }
     sort(c,c+N,cmp);
     for(int i=0;i<N;i++){
