4f3312ff33315fe230761c01da4b8ec79487b6db
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-14 14:00:29 +0900

    Title: DateTime to DateTime, Time: 1033ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 250714/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    cin >> a >> b >> c;
+    int hour=11;
+    int day=11;
+    int minute=11;
+    int elapse_time=0;
+    while(1){
+        if(day==a && hour==b && minute==c){
+            break;
+        }
+        elapse_time++;
+        minute++;
+        if(minute==60){
+            hour++;
+            minute=0;
+        }
+        if(hour==24){
+           day++;
+           minute=0;
+           hour=0; 
+        }
+    }
+    if(b<=11 && a<11){
+        cout << -1;
+    }
+    else
+    cout <<elapse_time;
+    // Please write your code here.
+
+    return 0;
+}
