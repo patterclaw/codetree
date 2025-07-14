db61db4686978f5e8e049d09d414c1a2914b619a
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-14 13:07:02 +0900

    Title: 정렬된 수 위치 알아내기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250714/정렬된 수 위치 알아내기/indices-of-sorted-array.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int sequence[1000];
+int score[1001];
+class Sequence{
+    public:
+    int element;
+    int number;
+    Sequence(int element,int number){
+        this->element=element;
+        this->number=number;
+    }
+    Sequence(){}
+};
+bool cmp(const Sequence &a,const Sequence &b){
+    return a.element<b.element;
+}
+int main() {
+    
+    cin >> N;
+    Sequence s[1000];
+    for (int i = 0; i < N; i++) {
+        cin >> sequence[i];
+        s[i]=Sequence(sequence[i],i+1);
+    }
+    sort(s,s+N,cmp);
+    for(int i=0;i<N;i++){
+        score[s[i].number]=i+1;
+    }
+    for(int a=1;a<=N;a++){
+        cout << score[a] <<  ' ';
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
