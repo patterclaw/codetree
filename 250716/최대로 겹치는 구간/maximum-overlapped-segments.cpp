17ac5dc669836fd21754b7d79d630f422859a1b5
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-16 13:05:11 +0900

    Title: 최대로 겹치는 구간, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 250716/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 250716/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -8,15 +8,17 @@ int cnt[100];
 
 int main() {
     cin >> n;
-    
+    int max_index=0;
     for (int i = 0; i < n; i++) {
         cin >> x1[i] >> x2[i];
+        if(x2[i]>max_index)
+        max_index=x2[i];
         for(int a=x1[i];a<x2[i];a++){
             cnt[a]++;
         }
     }
     int max=cnt[0];
-    for(int i=1;i<100;i++){
+    for(int i=1;i<max_index;i++){
         if(cnt[i]>max)
         max=cnt[i];
     }
