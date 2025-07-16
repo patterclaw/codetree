94d3037c9634608428d585ed7e5e0483745d9749
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-17 03:08:08 +0900

    Title: 사각형의 총 넓이 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250717/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
+++ 250717/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -11,10 +11,14 @@ int main() {
 
     for (int i = 0; i < N; i++) {
         cin >> x1[i] >> y1[i] >> x2[i] >> y2[i];
+        x1[i]+=100;
+        x2[i]+=100;
+        y1[i]+=100;
+        y2[i]+=100;
         for(int j=x1[i];j<x2[i];j++){
             for(int k=y1[i];k<y2[i];k++){
                 if(cnt[j][k]==0)
-                cnt[j][k]++;
+                cnt[j][k]=1;
             }
         }
     }
