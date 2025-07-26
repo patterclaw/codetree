a714f199e8f10d51eef3d19f16f4080147a2bbd2
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-26 20:04:31 +0900

    Title: 연속되는 수 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250726/연속되는 수 2/continuous-number2.cpp
+++ 250726/연속되는 수 2/continuous-number2.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 int N;
 int arr[1000];
 int cnt;
+int ans;
 int main() {
     cin >> N;
     for (int i = 0; i < N; i++) {
         cin >> arr[i];
     }
     for(int i=0;i<N;i++){
-        if(i==0 || arr[i]!=arr[i-1])
-        cnt++;
+        if(i>0 &&  arr[i]==arr[i-1])
+            cnt++;
+        else
+        cnt=1;
+        ans=max(ans,cnt);
     }
-    cout <<cnt;
+    cout <<ans;
     // Please write your code here.
 
     return 0;
