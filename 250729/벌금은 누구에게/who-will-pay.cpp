0f709a0d5b41ba91d5df7e1ba1f613c7f9b19c5d
patterclaw <163995564+patterclaw@users.noreply.github.com>
2025-07-29 06:40:08 +0900

    Title: 벌금은 누구에게, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250729/벌금은 누구에게/who-will-pay.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int n,m,k;
+int student[10000];
+
+int main() {
+    cin >> n >> m >> k;
+    int arr[100]={};
+    for (int i = 0; i < m; i++) {
+        cin >> student[i];
+        arr[student[i]]++;
+    }
+    int anw=-1;
+    for(int i=0;i<m;i++){
+        if(arr[student[i]]==k)
+        {
+            anw=student[i];
+            break;
+        }
+    }
+    cout <<anw;
+    // Please write your code here.
+
+    return 0;
+}
